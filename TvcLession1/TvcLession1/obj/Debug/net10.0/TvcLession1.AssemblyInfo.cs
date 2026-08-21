2bab33d16ecdc57a503a9d061d4dc65346c2fdee
nguyen phu <Nguyenphu310806@gmail.com>
2026-08-21 21:31:44 +0700

    TvcLesson1 - First webapp

--- TvcLession1/TvcLession1/obj/Debug/net10.0/TvcLession1.AssemblyInfo.cs
+++ TvcLession1/TvcLession1/obj/Debug/net10.0/TvcLession1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TvcLession1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f9c12c2d4784215863c7ce390e99441f826ca0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TvcLession1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TvcLession1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TvcLessionMVC/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TvcLessionMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
